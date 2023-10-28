af94d9ddb1e127bbc66a867ced999d15ba143eb7
Alcantar100206 <149212536+Alcantar100206@users.noreply.github.com>
2023-10-27 18:17:48 -0600

    Add files via upload

---
+++ inicio de sesion.js
@@ -0,0 +1,8 @@
+function ir(){
+    var c=123;
+    var u="AngelAlcantar@123";
+    if(document.form.password.value==c && document.form.login.value==u){
+        window.location="Pagina principal.html";}   
+    else{
+        alert("Ingresa nombre de usuario y contraseña correcta")}
+    }
